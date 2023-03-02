3cdb5f18fd4f3b3bd94bab5424ddcac3947face5
Kaikyu <kaikyu.lotus@gmail.com>
2023-03-03 00:16:08 +0100

    Remove unused import

--- lib/widgets/character_widget.dart
+++ lib/widgets/character_widget.dart
@@ -1,6 +1,5 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
 import 'package:genshin_renderer/entities/constants.dart';
 import 'package:genshin_renderer/entities/genshin.dart';
 
